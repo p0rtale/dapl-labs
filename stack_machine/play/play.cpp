877356fd6aae2fab478a54c870fc8e828f962c5a
p0rtale <petryaev_1984@mail.ru>
2023-03-04 16:30:45 +0300

    play: command line args

--- stack_machine/play/play.cpp
+++ stack_machine/play/play.cpp
@@ -1,41 +1,24 @@
 #include <iostream>
 
-#include <stack_machine/machine/Machine.h>
-#include <stack_machine/asm_compiler/Lexer.h>
 #include <stack_machine/asm_compiler/Compiler.h>
-
-using namespace asm_compiler;
-using namespace stack_machine;
-
-int main() {
-    // std::ifstream fin("test.txt");
-    // Lexer lex{fin};
-    // auto ok = lex.scan();
-
-    // if (!ok) {
-    //     return EXIT_FAILURE;
-    // }
-
-    // auto token = Token{TokenType::kBlank, ""};
-    // do {
-    //     token = lex.nextToken();
-    //     std::cout << static_cast<int>(token.type) << ": " << token.value << "\n";
-    // } while (!token.isEmpty());
+#include <stack_machine/machine/Machine.h>
 
 
-    // Compiler compiler{std::vector<std::string>{"sum.txt"}};
-    // bool ok = compiler.compile();
-    // if (!ok) {
-    //     std::cout << "BAN!\n";
-    //     return EXIT_FAILURE;
-    // }
+int main(int argc, char* argv[]) {
+    size_t filesNum = argc - 1;
+    std::vector<std::string> files(argv + 1, argv + 1 + (filesNum));    
+    asm_compiler::Compiler compiler{files};
 
-    // compiler.saveMachineCode("machine_code.txt");
+    bool ok = compiler.compile();
+    if (!ok) {
+        return EXIT_FAILURE;
+    }
 
-    StackMachine machine{1000, std::cin, std::cout};
+    compiler.saveMachineCode("machine_code.txt");
+
+    stack_machine::StackMachine machine{100000, std::cin, std::cout};
 
     std::ifstream fin("machine_code.txt");
-
     machine.run(fin);
 
     std::cout << "\n";
