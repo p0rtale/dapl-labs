91d65ce8bf94e77e80a624236119423129acfe77
p0rtale <petryaev_1984@mail.ru>
2023-12-26 14:18:26 +0300

    Clipl: remove comments

--- Clipl/Clipl/Grammar/Lexer/Lexer.hpp
+++ Clipl/Clipl/Grammar/Lexer/Lexer.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-// #if !defined(yyFlexLexerOnce)
 #define yyFlexLexer cliplFlexLexer
 #include <FlexLexer.h>
 #undef yyFlexLexer
-// #endif
 
 #include <Clipl/Grammar/Lexer/LexerInternal.hpp>
--- Clipl/Clipl/Preprocessor/Lexer/Lexer.hpp
+++ Clipl/Clipl/Preprocessor/Lexer/Lexer.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-// #if !defined(yyFlexLexerOnce)
 #define yyFlexLexer preFlexLexer
 #include <FlexLexer.h>
 #undef yyFlexLexer
-// #endif
 
 #include <Clipl/Preprocessor/Lexer/LexerInternal.hpp>
