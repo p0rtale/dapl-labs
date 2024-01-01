ee39b6c21d546aebaf04fa621d46ca19670f8fc7
p0rtale <petryaev_1984@mail.ru>
2024-01-01 20:48:30 +0300

    Clipl (AST): add pointer

--- Clipl/Clipl/Grammar/AST/Program/Pointer.hpp
+++ Clipl/Clipl/Grammar/AST/Program/Pointer.hpp
@@ -1,15 +1,24 @@
 #pragma once
 
 #include <Clipl/Grammar/AST/Base/ASTNode.hpp>
-
+#include <Clipl/Grammar/AST/Program/TypeQualifier.hpp>
 
 namespace ast {
 
 class Pointer: public ASTNode {
 public:
-    Pointer();
+    Pointer() = default;
+
+    Pointer(std::vector<std::shared_ptr<TypeQualifier>> typeQualifiers,
+            std::shared_ptr<Pointer> pointer = nullptr)
+        : m_TypeQualifiers(std::move(typeQualifiers)),
+          m_Tail(pointer) {}
 
     // void accept(Visitor *visitor) override;
+
+private:
+    std::vector<std::shared_ptr<TypeQualifier>> m_TypeQualifiers;
+    std::shared_ptr<Pointer> m_Tail = nullptr;
 };
 
 }  // namespace ast
