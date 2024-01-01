e2caa71d171268f5b4a6ffd94ceb49de27c166fe
p0rtale <petryaev_1984@mail.ru>
2024-01-01 20:49:33 +0300

    Clipl (AST): add parameter type list

--- Clipl/Clipl/Grammar/AST/Program/ParameterTypeList.hpp
+++ Clipl/Clipl/Grammar/AST/Program/ParameterTypeList.hpp
@@ -1,15 +1,23 @@
 #pragma once
 
 #include <Clipl/Grammar/AST/Base/ASTNode.hpp>
+#include <Clipl/Grammar/AST/Program/ParameterDeclaration.hpp>
 
 
 namespace ast {
 
 class ParameterTypeList: public ASTNode {
 public:
-    ParameterTypeList();
+    ParameterTypeList(std::vector<std::shared_ptr<ParameterDeclaration>> parameters,
+                      bool haveEllipsis = false)
+        : m_Parameters(std::move(parameters)),
+          m_HaveEllipsis(haveEllipsis) {}
 
     // void accept(Visitor *visitor) override;
+
+private:
+    std::vector<std::shared_ptr<ParameterDeclaration>> m_Parameters;
+    bool m_HaveEllipsis = false;
 };
 
 }  // namespace ast
